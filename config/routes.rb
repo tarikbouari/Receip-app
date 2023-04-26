eadf58d0359b86f8c59a695372c686a224702483
Bludidi <bludidi5@gmail.com>
2023-04-26 17:32:09 +0200

    Add style for recipe index

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,16 @@ Rails.application.routes.draw do
   get 'recipes/new'
   get 'recipes/create'
   get 'recipes/destroy'
+  # post '/users/:user_id/recipes/new', to: 'recipes#create', as: 'create_recipe'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
-
+  resources :users do
+    resources :foods
+    resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+      resources :recipe_foods
+    end
+  end
 end
