62d3b5ee62975b747a6966116e825e089aa1d810
Bludidi <bludidi5@gmail.com>
2023-04-26 14:51:03 +0200

    Generate recipe controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
