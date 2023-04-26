78e452d96affca8b40b8526f50b50ff40b199721
tarikbouari <tarikbouari@gmail.com>
2023-04-26 13:42:20 +0000

    Display data from  food table

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,40 @@
+class FoodsController < ApplicationController
+    def index
+        @foods = Food.all
+    end
+
+    def new 
+        @new_food = food.new
+    end
+
+    def destroy
+        food = Food.find(params[:id])
+        if food.destroy
+            flash.now[:notice] = 'food was successfully destroyed'
+        else
+            flash.now[:alert] = 'food could not be removed'
+        end
+        redirect_to user_foods_url
+    end
+
+
+    def create
+        @food = Food.new(food_params)
+
+        respond_to do |format|
+            format.html do
+                if @food.save!
+                    redirect_to user_foods_url
+                else
+                    redirect_to new_user_food_path(current_user)
+                end
+            end
+        end
+    end
+
+
+
+    def food_params
+        params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user_id: current_user.id)
+    end
+end
