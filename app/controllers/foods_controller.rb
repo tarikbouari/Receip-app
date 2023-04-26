62e315b8e161f5c08cd319fa4cd7992c71d43db9
tarikbouari <tarikbouari@gmail.com>
2023-04-26 16:53:02 +0000

    fix linters error

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,40 +1,37 @@
 class FoodsController < ApplicationController
-    def index
-        @foods = Food.all
-    end
+  def index
+    @foods = Food.all
+  end
 
-    def new 
-        @new_food = Food.new
-    end
+  def new
+    @new_food = Food.new
+  end
 
-    def destroy
-        food = Food.find(params[:id])
-        if food.destroy
-            flash.now[:notice] = 'food was successfully destroyed'
+  def destroy
+    food = Food.find(params[:id])
+    if food.destroy
+      flash.now[:notice] = 'food was successfully destroyed'
+    else
+      flash.now[:alert] = 'food could not be removed'
+    end
+    redirect_to user_foods_url
+  end
+
+  def create
+    @food = Food.new(food_params)
+
+    respond_to do |format|
+      format.html do
+        if @food.save!
+          redirect_to user_foods_url
         else
-            flash.now[:alert] = 'food could not be removed'
+          redirect_to new_user_food_path(current_user)
         end
-        redirect_to user_foods_url
+      end
     end
+  end
 
-
-    def create
-        @food = Food.new(food_params)
-
-        respond_to do |format|
-            format.html do
-                if @food.save!
-                    redirect_to user_foods_url
-                else
-                    redirect_to new_user_food_path(current_user)
-                end
-            end
-        end
-    end
-
-
-
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user_id: current_user.id)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user_id: current_user.id)
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,12 +1,12 @@
 class Food < ApplicationRecord
-  #validation 
+  # validation
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :user_id, presence: true
 
-  #association 
+  # association
   belongs_to :user, class_name: 'User'
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods, dependent: :destroy
