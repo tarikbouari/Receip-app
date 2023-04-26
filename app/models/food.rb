13568fdf04ba04a4eaa68372c3bae6a0e5d4c6c7
tarikbouari <tarikbouari@gmail.com>
2023-04-26 10:53:50 +0000

    fix linters error

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-    belongs_to :user
-    has_many :recipes_foods, dependent: :destroy
-    has_many :recipes, through: :recipe_foods, dependent: :destroy
+  belongs_to :user
+  has_many :recipes_foods, dependent: :destroy
+  has_many :recipes, through: :recipe_foods, dependent: :destroy
 end
