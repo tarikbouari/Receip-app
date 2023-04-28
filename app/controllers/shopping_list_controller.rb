3ad39b6e3ec0efc2bf92bfbb6a30d0dad51db16f
tarikbouari <tarikbouari@gmail.com>
2023-04-28 20:15:38 +0000

    update readme for final submission

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,11 +1,14 @@
 class ShoppingListController < ApplicationController
   def index
     @recipes = Recipe.includes(:recipe_foods).where(user: current_user)
+
     @amount_of_food = 0
+    
     @recipes.each do |recipe|
       @amount_of_food += recipe.recipe_foods.count
     end
     @total = 0
+
     @recipes.each do |recipe|
       recipe.recipe_foods.each do |recipe_food|
         @total += recipe_food.food.price * recipe_food.quantity
@@ -16,6 +19,6 @@ class ShoppingListController < ApplicationController
     @recipes.each do |recipe|
       @recipe_foods_arr.concat(recipe.recipe_foods)
     end
-    @recipe_foods_arr
+  
   end
 end
